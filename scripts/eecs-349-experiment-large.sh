85351596ce031aed2bb0448c3a1d0762337f7bab
chits22 <chits22@gmail.com>
2018-05-17 20:40:44 -0500

    changed port

--- scripts/eecs-349-experiment-large.sh
+++ scripts/eecs-349-experiment-large.sh
@@ -6,5 +6,6 @@ python train.py \
        --model="rnn" \
        --batch_size=64 \
        --output_dir=large \
+       --dropout=0.1,0.3,0.5 \
 
-tensorboard --logdir=large/tensorboard_log/ --port=6007
+tensorboard --logdir=large/tensorboard_log/ --port=6008
